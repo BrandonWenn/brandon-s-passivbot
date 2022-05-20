cb857e3a0a94cbd742d10451c3ecc8c3c8e8b1dc
brandon <1656830314@qq.com>
2022-05-20 16:56:21 +0800

    V5.5.2

---
+++ test.bat
@@ -0,0 +1,3 @@
+C:\py3810\python.exe inspect_opt_results.py C:\Users\Administrator\Desktop\passivbot\backtests\binance\ZILUSDT\plots\2022-05-15T132220\backtest_result.txt
+
+pause
---
+++ 管理manager.bat
@@ -0,0 +1,3 @@
+python manager start -a
+
+pause
